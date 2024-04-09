41af88f36f1e10518ddce469f119bb6dbf560d21
Abigael Kirwa <abigael.kirwa@strathmore.edu>
2024-04-09 22:52:09 +0300

    fix(about section):change structure of about section

--- src/pages/landingPage/About.tsx
+++ src/pages/landingPage/About.tsx
@@ -15,28 +15,15 @@ const About = () => {
             to reach home bakers. Using our application, you can shop from multiple 
             home bakers and have your goods delivered in one go! Planning for events 
             and personal shopping has never been easier!
+            Choose from different categories of confectioneries and pick the specific flavour and baker 
+            you would like to order from, and add the item to cart. You can select items from different 
+            vendors and place them all in your cart. Order and pay for the items and voila! We will deliver it to you.
           </Typography>
         </Grid> 
         <Grid item md={6} sx={{display:{md:"flex", sm:"none", xs:'none'}, justifyContent:"flex-end"}}>
           <div>
             <img src={AboutPlatter} style={{width:"100%"}}/>
           </div>
-        </Grid>
-      </Grid>
-      <Grid container spacing={{md:-2}} columns={{md:12, sm:8, xs:8}} display='flex' alignItems='center' justifyContent='center' sx={{mt:{md:'-20px', sm:'20px', xs:'25px'}}}>
-        <Grid item md={6} sx={{display:{md:"flex", sm:"none", xs:'none'}}}>
-          <div>
-            <img src={AboutCake} style={{width:"95%"}}/>
-          </div>
-        </Grid>
-        <Grid item md={6} sm={6} xs={6} sx={{display:"flex"}}>
-          <Typography textAlign='justify' lineHeight='50px' pr='4em' sx={{pr:{md:'4em', sm:'0'}}}>
-            Oka bakery is your one stop online shop for all baked products. 
-            Gone are the days where you had to use multiple social media apps 
-            to reach home bakers. Using our application, you can shop from multiple 
-            home bakers and have your goods delivered in one go! Planning for events 
-            and personal shopping has never been easier!
-          </Typography>
         </Grid>
       </Grid>
     </div>
